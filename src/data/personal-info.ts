03b29a567eaafc51d4406d84207b05cde37c4020
bahauddinchishte2 <sharifuddinchishte@gmail.com>
2025-01-03 05:33:28 +0600

    added images and link

--- src/data/personal-info.ts
+++ src/data/personal-info.ts
@@ -13,20 +13,19 @@ export interface PersonalInfo {
 
 export const personalInfo: PersonalInfo = {
   name: 'Baha',
-  role: 'Software Engineer',
-  bio: "I'm a software engineer specializing in building exceptional digital experiences. Currently focused on building accessible, human-centered products.",
+  role: 'Tech Entrepreneur',
+  bio: "I'm a tech entrepreneur who loves to solve real world problem using technology. Aside tech I love to work on marketing, product and business development",
   images: [
     'https://avatars.githubusercontent.com/u/91181868?v=4',
     'https://live.staticflickr.com/65535/54241877452_39dc62951f_k.jpg',
     'https://live.staticflickr.com/65535/54242780096_6fd0e9f8cd_k.jpg',
     'https://live.staticflickr.com/65535/54242779836_4c08f24151_k.jpg'
     
-    
   ],
   contact: {
     email: 'bahauddin.co@gmail.com',
     phone: '+880 1621 28 5057',
     github: 'https://github.com',
-    linkedin: 'https://linkedin.com',
+    linkedin: 'https://linkedin.com/bahauddin-chishte',
   },
 };
